c15233bac7a5faad8b5f3b4c85a94c8495fefe5e
04Mansigarg <garg6446@gmail.com>
2022-03-28 22:29:19 +0530

    video

--- react-routing/src/components/Home.jsx
+++ react-routing/src/components/Home.jsx
@@ -3,8 +3,9 @@ import { AuthContext } from '../context/AuthContext'
 
 export const Home = () => {
   const [data, setData] = React.useState([])
-  const token = useContext(AuthContext)
+  const { token } = useContext(AuthContext)
   console.log(token)
+
   const getData = () => {
     fetch("https://masai-api-mocker.herokuapp.com/user/johnfly", {
       method: "GET",
@@ -13,18 +14,21 @@ export const Home = () => {
       .then((res) => res.json())
       .then((res) => {
         setData(res)
-        console.log(res)
+
       })
       .catch((err) => console.log(err))
   }
-  React.useState((
-    getData()
+  React.useEffect((
+    getData
+
+
   ), [])
 
 
   return (
     <div>
       <h1>User Info</h1>
+      <h1>{token}</h1>
       {data.map((item) => {
         return (
           <h1>{item.name}</h1>
--- react-routing/src/context/AuthContext.jsx
+++ react-routing/src/context/AuthContext.jsx
@@ -3,19 +3,19 @@ export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
     const [isAuth, setAuth] = React.useState(false)
-    const [token, setToken] = React.useState()
+    const [token, setToken] = React.useState(null)
 
     const login = (token) => {
         setAuth(true)
         setToken(token)
-        console.log(token)
+
 
     }
     const logout = () => {
         setAuth(false)
     }
 
-    const value = { token, isAuth, login, logout }
+    const value = { token, isAuth, login, logout, setToken }
     return (
         <AuthContext.Provider value={value}>{children}</AuthContext.Provider>)
 }
